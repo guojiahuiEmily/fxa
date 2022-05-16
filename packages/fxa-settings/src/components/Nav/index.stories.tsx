20794b6f7bf449403cf18cef10ba64060fae6920
Mill <msoper@mozilla.com>
2022-05-16 13:00:46 -0700

    chore(storybook) add LinkedAccounts to stories
    chore(storybook) refactor nav stories
    
    fix(storybook) use addon to get ltr/rtl in storybook

--- packages/fxa-settings/src/components/Nav/index.stories.tsx
+++ packages/fxa-settings/src/components/Nav/index.stories.tsx
@@ -3,33 +3,67 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { mockSession } from '../../models/mocks';
+import { LocationProvider } from '@reach/router';
 import { getDefault } from '../../lib/config';
 import { Nav } from '.';
 import { AppContext } from 'fxa-settings/src/models';
+import { mockAppContext } from '../../models/mocks';
+import { Account } from '../../models/Account';
+import { MOCK_LINKED_ACCOUNTS } from '../LinkedAccounts/mocks';
+
+const defaultStory = { title: 'Components/Nav', component: Nav };
+export default defaultStory;
 
 const account = {
   primaryEmail: {
     email: 'johndope@example.com',
   },
   subscriptions: [{ created: 1, productName: 'x' }],
+  linkedAccounts: [],
 } as any;
-storiesOf('Components/Nav', module)
-  .add('basic', () => <Nav />)
-  .add('with link to Subscriptions', () => (
-    <AppContext.Provider value={{ account, session: mockSession() }}>
-      <Nav />
-    </AppContext.Provider>
-  ))
-  .add('without link to Newsletters', () => {
-    const config = Object.assign({}, getDefault(), {
-      marketingEmailPreferencesUrl: '',
-    });
 
-    return (
-      <AppContext.Provider value={{ account, session: mockSession(), config }}>
+const accountWithLinkedAccounts = Object.assign({}, account, {
+  linkedAccounts: MOCK_LINKED_ACCOUNTS,
+});
+
+const configWithoutNewsletterLink = Object.assign({}, getDefault(), {
+  marketingEmailPreferencesUrl: '',
+});
+
+const storyWithAccount = (
+  account: Partial<Account>,
+  storyName?: string,
+  config?: any
+) => {
+  const context = config
+    ? { account: account as Account, config: config }
+    : { account: account as Account };
+
+  const story = () => (
+    <LocationProvider>
+      <AppContext.Provider value={mockAppContext(context)}>
         <Nav />
       </AppContext.Provider>
-    );
-  });
+    </LocationProvider>
+  );
+  if (storyName) story.storyName = storyName;
+  return story;
+};
+
+export const basic = () => <Nav />;
+
+export const WithLinkToSubscriptions = storyWithAccount(
+  account,
+  'with link to Subscriptions'
+);
+
+export const WithLinkedAccounts = storyWithAccount(
+  accountWithLinkedAccounts,
+  'with linked accounts'
+);
+
+export const WithoutNewsletterLink = storyWithAccount(
+  account,
+  'without link to Newsletters',
+  configWithoutNewsletterLink
+);
